8ffd1bfb2abfbb8da9e8711ff678d85f7984240f
Hazim Sami <hazimisnot@gmail.com>
2020-11-18 13:00:33 +0000

    fully working check calculator

--- check_please.py
+++ check_please.py
@@ -1,6 +1,11 @@
+import math
 def split_check(total, number_of_people):
-  cost_per_person = total / number_of_people
-  return cost_per_person
+  cost_per_person = total_due / split_among
+  # we want to round up the number in order to make sure that the person paying is not paying the extra out of their own pocket
+  return math.ceil(cost_per_person)
 
-amount_due = split_check(84.97, 4)
+
+total_due = float(input("What is the total? \n"))
+split_among = int(input("How many people are we spliting it among? \n"))
+amount_due = split_check(total_due, split_among)
 print(f'Each person owes ${amount_due}')
