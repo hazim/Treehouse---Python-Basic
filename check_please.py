824375b3877638abe92427dd9fcd8ae0afd6bb71
Hazim Sami <hazimisnot@gmail.com>
2020-11-18 13:18:00 +0000

    function cleanup

--- check_please.py
+++ check_please.py
@@ -1,8 +1,7 @@
 import math
 def split_check(total, number_of_people):
-  cost_per_person = total_due / split_among
   # we want to round up the number in order to make sure that the person paying is not paying the extra out of their own pocket
-  return math.ceil(cost_per_person)
+  return math.ceil(total_due / split_among)
 
 
 total_due = float(input("What is the total? \n"))
